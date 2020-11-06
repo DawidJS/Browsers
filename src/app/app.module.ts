f46bc23e7481ccdc95df3490eb440028b1295040
DawidJS <48735427+DawidJS@users.noreply.github.com>
2020-11-06 08:25:37 +0100

    90%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbdTabsetBasicComponent } from './ngbd-tabset-basic/ngbd-tabset-basic.component';
+import { NgbdAccordionBasicComponent } from './ngbd-accordion-basic/ngbd-accordion-basic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbdTabsetBasicComponent,
+    NgbdAccordionBasicComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgbPaginationModule,
+    NgbAlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ngbd-tabset-basic/ngbd-tabset-basic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngbd-tabset-basic',
+  templateUrl: './ngbd-tabset-basic.component.html',
+  styleUrls: ['./ngbd-tabset-basic.component.less']
+})
+export class NgbdTabsetBasicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
